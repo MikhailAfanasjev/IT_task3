5c2a968a16b2e5fc5ef2bbde56375d6c9466bb55
MikhailAfanasjev <66779383+MikhailAfanasjev@users.noreply.github.com>
2021-10-29 09:18:45 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Ifile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    interface IFile
+    {
+        void CreateFile(String Name);
+        void RenameFile(String OldName, String NewName);
+        string GetContent(String Name);
+        void DeleteFile(String Name);
+        void AddToFile(String Name, String Content);
+    }
+
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/TextFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    class TextFile : IFile, IDirectory
+    {
+        public void CreateDirectory(String Name)
+        {
+            Directory.CreateDirectory(Name);
+        }
+        public void RenameDirectory(String OldName, String NewName)
+        {
+            Directory.Move(OldName, NewName);
+        }
+        public void DeleteDirectory(String Name)
+        {
+            Directory.Delete(Name);
+        }
+        public void CreateFile(String Name)
+        {
+            if (!File.Exists(Name))
+                File.Create(Name);
+        }
+        public void RenameFile(String OldName, String NewName)
+        {
+            File.Move(OldName, NewName);
+        }
+        public void DeleteFile(String Name)
+        {
+            File.Delete(Name);
+        }
+        public void AddToFile(String Name, String Content)
+        {
+            File.AppendAllText(Name, Content);
+        }
+        public string GetContent(String Name)
+        {
+            return File.ReadAllText(Name);
+        }
+    }
+}
